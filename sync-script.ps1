747d2891b63c2c5f8d43a3fa7a3ae2b7f37b6cd4
neetha.oommen <neetha.oommen@outlook.com>
2023-02-24 18:28:34 +0000

    Updated sync-script.ps1

--- sync-script.ps1
+++ sync-script.ps1
@@ -11,7 +11,7 @@ $destination = $githubDir +"\"+"flatris.git"
 $alias = 'snowwhite686:'+ "$($env:Github.PAT)"
 $azurepat = "$($env:AzureDevOps.PAT)"
 #Please make sure, you remove https from azure-repo-clone-url
-$sourceURL = 'https://+'$azurepat'+'@icloud9/Features%20Rich%20Epics%20Team/_git/flatriss.git'
+$sourceURL = 'https://icloud9@dev.azure.com/icloud9/Features%20Rich%20Epics%20Team/_git/flatriss.git'
 #Please make sure, you remove https from github-repo-clone-url
 $destURL = 'https://' + $alias + '@snowwhite686/flatrissbck.git'
 #Check if the parent directory exists and delete
