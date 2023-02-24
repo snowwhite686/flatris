ea971b39bfc50fb1c7f2eebfd80dc630024d7400
neetha.oommen <neetha.oommen@outlook.com>
2023-02-24 20:14:26 +0000

    Updated sync-script.ps1

--- sync-script.ps1
+++ sync-script.ps1
@@ -9,7 +9,7 @@ $githubDir = $stageDir +"\"+"gitHub"
 $destination = $githubDir +"\"+"flatriss.git"
 #please provide your username
 #$alias = 'snowwhite686:'+ "$($env:Github.PAT)"
-$alias = 'snowwhite686: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'
+$alias = 'snowwhite686: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'
 $azurepat = "$($env:AzureDevOps.PAT)"
 #Please make sure, you remove https from azure-repo-clone-url
 $sourceURL = 'https://icloud9@dev.azure.com/icloud9/Features%20Rich%20Epics%20Team/_git/flatriss.git'
